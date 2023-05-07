57e68ae9fd35f128d590cd8c1563febe39bdc121
Justin Tucker <justin.tucker997@gmail.com>
2023-05-06 23:03:26 -0700

    update ch10

--- ch10/cookies.js
+++ ch10/cookies.js
@@ -15,16 +15,25 @@ function createCookie(name, values, days, path, domain, secure) {
 
 
 function getCookie(name) {
-	var nameEquals = name + "=";
-	var crumbs = document.cookie.split(';');
-	for (var i = 0; i < crumbs.length; i++) {
-		var crumb = crumbs[i];
-		if (crumb.indexOf(nameEquals) == 0) {
-			return unescape(crumb.substring(nameEquals.length, crumb.length));
-		}
-	}
-	return null;
-}
+    var nameEquals = name + "=";
+    var crumbs = document.cookie.split(';');
+    for (var i = 0; i < crumbs.length; i++) {
+      var crumb = crumbs[i];
+      while (crumb.charAt(0) == ' ') {
+        crumb = crumb.substring(1, crumb.length);
+      }
+      if (crumb.indexOf(nameEquals) == 0) {
+        var value = unescape(crumb.substring(nameEquals.length, crumb.length));
+        if (value.indexOf("#") != -1) {
+          return value.split("#");
+        } else {
+          return [value];
+        }
+      }
+    }
+    return null;
+  }
+  
 
 function deleteCookie(name) {
 	createCookie(name,"",-1);

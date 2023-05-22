8e5dceb43f19ca2fcb1e95403d5101f7d750ae55
Justin Tucker <justin.tucker997@gmail.com>
2023-05-22 16:34:01 -0700

    ch 16 update

--- ch16/ajax.js
+++ ch16/ajax.js
@@ -12,24 +12,22 @@ function getXMLHttpRequest() {
 	}
 }
 function callAjax() {
-    var url = "clock.php";
-    var myRandom = parseInt(Math.random() * 99999999);
-    myRequest.open("GET", url + "?rand=" + myRandom, true);
-    myRequest.onreadystatechange = responseAjax;
-    myRequest.send(null);
+	var url = "clock.php";
+	var myRandom = parseInt(Math.random()*99999999);
+	myRequest. open("GET", url + "?rand=" + myRandom, true);
+	myRequest.onreadystatechange = responseAjax;
+	myRequest.send(null);
 }
-
 function responseAjax() {
-    if (myRequest.readyState == 4) {
-        if (myRequest.status == 200) {
-            var serverTime = myRequest.responseText;
-            var localTime = new Date();
-            
-            document.getElementById("clock").innerHTML = "Server: " + serverTime + "<br />Local: " + localTime.toLocaleTimeString();
+    if(myRequest.readyState == 4) {
+        if(myRequest.status == 200) {
+			var now = new Date();
+			var localTime = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+			var serverTime = myRequest.responseText;
+            document.getElementById("clock").innerHTML = "Server: " + serverTime + "<br />Local: " + localTime; 
         } else {
             alert("An error has occurred: " + myRequest.statusText);
         }
     }
 }
-
 var myRequest = getXMLHttpRequest();
